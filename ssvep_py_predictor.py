16e43812e54c26b9c658bea8d4b083d6cc233f8b
okba bekhelifi <okba.bekhelifi@gmail.com>
2019-04-09 23:38:06 +0200

    - Finished SSVEP py predictor 	- Finished CCA  Changes to be committed: 	modified:   ssvep_py_predictor.py

--- ssvep_py_predictor.py
+++ ssvep_py_predictor.py
@@ -1,8 +1,38 @@
 from __future__ import print_function, division
 from sklearn.cross_decomposition import CCA
+from scipy.linalg import eig
+from scipy import sqrt
 import numpy as np
 import pickle
 
+def cca(X,Y):
+    if X.shape[1] != Y.shape[1]:
+        raise Exception('unable to apply CCA, X and Y have different dimensions')
+    z = np.vstack((X,Y))
+    C = np.cov(z)
+    sx = X.shape[0]
+    sy = Y.shape[0]
+    Cxx = C[0:sx, 0:sx] + 10**(-8)*np.eye(sx)
+    Cxy = C[0:sx, sx:sx+sy]
+    Cyx = Cxy.transpose()
+    Cyy = C[sx:sx+sy, sx:sx+sy] + 10**(-8)*np.eye(sy)
+    invCyy = np.linalg.pinv(Cyy)
+    invCxx = np.linalg.pinv(Cxx)
+    r, Wx = eig(invCxx.dot(Cxy).dot(invCyy).dot(Cyx))
+    r = sqrt(np.real(r))
+    r = np.sort(np.real(r),  axis=None)
+    r = np.flipud(r)
+    return r
+
+def apply_cca(X,Y):
+    coefs = []
+    for i in range(Y.shape[0]):
+        coefs.append(cca(X,Y[i,:,:]))
+    coefs = np.array(coefs).transpose()
+    return coefs
+
+def predict(scores):
+    return np.argmax(scores[0,:])
 
 
 class SSVEPpredictor(OVBox):
@@ -29,8 +59,9 @@ class SSVEPpredictor(OVBox):
         if self.frequencies[0] == 'idle':
             frequencies = self.frequencies[1:]
         # generate reference signals
-        x = [[np.cos(2*np.pi*f*t),np.sin(2*np.pi*f*t)] for f in frequencies]
-        self.references = np.array(x).reshape(self.num_harmonics * len(frequencies), int(samples))  
+        x = [ [np.cos(2*np.pi*f*t*i),np.sin(2*np.pi*f*t*i)] for f in frequencies for i in range(1, self.num_harmonics+1)]
+        # self.references = np.array(x).reshape(self.num_harmonics * len(frequencies), int(samples))
+        self.references = np.array(x).reshape(len(frequencies), 2*self.num_harmonics, int(samples))  
         
 
     
@@ -48,9 +79,13 @@ class SSVEPpredictor(OVBox):
             buffer = self.input[0].pop()
             if type(buffer) == OVSignalBuffer:
                 if (buffer):
-                    epoch = np.array(buffer)
-                    # model = CCA(n_component=self.num_harmonics)
-                    model = CCA(n_components=self.num_harmonics).fit(epoch, self.references).transform(epoch)
+                    samples = int(self.epoch_duration * self.fs)
+                    channels = int(len(buffer) / samples)
+                    epoch = np.array(buffer).reshape(channels, samples)
+                    r = apply_cca(epoch, self.references)
+                    command = predict(r)
+                    print('Frequency detected %s Hz' %(self.frequencies[command+1]))
+          
                     
 
 
