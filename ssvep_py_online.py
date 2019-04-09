75cd8884a69448436c438be9ea0161d24335a4a2
okba bekhelifi <okba.bekhelifi@gmail.com>
2019-04-10 00:27:27 +0200

    - Added SSVEP online py script
     On branch master
     Changes to be committed:
    	new file:   ssvep_py_online.py
    	modified:   ssvep_py_predictor.py

--- ssvep_py_online.py
+++ ssvep_py_online.py
@@ -4,6 +4,7 @@ from scipy.linalg import eig
 from scipy import sqrt
 import numpy as np
 import pickle
+import socket
 
 def cca(X,Y):
     if X.shape[1] != Y.shape[1]:
@@ -43,11 +44,16 @@ class SSVEPpredictor(OVBox):
         self.predictions = []
         self.events = []
         self.trials_count = 0
-        self.frequencies = ['idle', 6.66, 7.5, 8.57, 10]
+        self.frequencies = ['idle', 7, 9, 11, 13]
         self.num_harmonics = 0
         self.epoch_duration = 0
         self.fs = 512
-        self.references = []      
+        self.references = []
+        self.command = None
+        self.hostname = '127.0.0.1'
+        self.feedback_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.feedback_port = 12345   
+        self.do_feedback = False 
 
 
     def initialize(self):
@@ -60,7 +66,6 @@ class SSVEPpredictor(OVBox):
             frequencies = self.frequencies[1:]
         # generate reference signals
         x = [ [np.cos(2*np.pi*f*t*i),np.sin(2*np.pi*f*t*i)] for f in frequencies for i in range(1, self.num_harmonics+1)]
-        # self.references = np.array(x).reshape(self.num_harmonics * len(frequencies), int(samples))
         self.references = np.array(x).reshape(len(frequencies), 2*self.num_harmonics, int(samples))  
         
 
@@ -75,6 +80,19 @@ class SSVEPpredictor(OVBox):
                         stim = chunk.pop()
                         print('Received Marker: ', stim.identifier, 'stamped at', stim.date, 's')
 
+                        if stim.identifier ==  OpenViBE_stimulation['OVTK_StimulationId_TrialStart']:
+                            print('Trial Start...')
+                            self.command = None                             
+                            self.trials_count += 1
+                        
+                        if stim.identifier == OpenViBE_stimulation['OVTK_StimulationId_TrialStop']:
+                            print('Trial Stop... sending feedback')
+                            # do feedback
+                            if self.do_feedback:
+                                self.feedback_socket.sendto(self.command, (self.hostname, self.feedback_port))
+                                self.do_feedback = False
+                          
+
         if self.input[0]:
             buffer = self.input[0].pop()
             if type(buffer) == OVSignalBuffer:
@@ -84,7 +102,9 @@ class SSVEPpredictor(OVBox):
                     epoch = np.array(buffer).reshape(channels, samples)
                     r = apply_cca(epoch, self.references)
                     command = predict(r)
-                    print('Frequency detected %s Hz' %(self.frequencies[command+1]))  
+                    self.command = str(self.frequencies[command+1])
+                    self.do_feedback = True
+                    print('Frequency detected %s Hz' %(self.command))  
                     
 
 
