0d2b9a11a45cbe7b8300054d294c49a9334985b6
okba bekhelifi <okba.bekhelifi@gmail.com>
2019-04-15 00:45:11 +0200

    - Fixed sync issue 	- TrialStop event received before epoching is completed 	- Sending null values to stimulator process
     On branch master
     Changes to be committed:
    	modified:   ssvep_py_online.py

--- ssvep_py_online.py
+++ ssvep_py_online.py
@@ -5,6 +5,10 @@ from scipy import sqrt
 import numpy as np
 import pickle
 import socket
+import logging
+import os
+
+
 
 def cca(X,Y):
     if X.shape[1] != Y.shape[1]:
@@ -48,25 +52,34 @@ class SSVEPpredictor(OVBox):
         self.num_harmonics = 0
         self.epoch_duration = 0
         self.fs = 512
+        self.samples = 0
         self.references = []
         self.command = None
         self.hostname = '127.0.0.1'
         self.feedback_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.feedback_port = 12345   
         self.do_feedback = False 
+        self.trial_ended = False
 
 
     def initialize(self):
+        # config logging
+        logFile = self.setting['logfile']
+        open(logFile, "w").close()        
+        logging.basicConfig(filename=logFile, filemode='w',
+                            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                            level=logging.INFO)
+        #
         self.epoch_duration = float(self.setting['Epoch_duration'])
         self.num_harmonics = int(self.setting['Harmonics'])
         self.fs = float(self.setting['Sample_rate'])
-        samples = self.epoch_duration * self.fs
-        t = np.arange(0.0, samples) / self.fs
+        self.samples = int(self.epoch_duration * self.fs)
+        t = np.arange(0.0, float(self.samples)) / self.fs
         if self.frequencies[0] == 'idle':
             frequencies = self.frequencies[1:]
         # generate reference signals
         x = [ [np.cos(2*np.pi*f*t*i),np.sin(2*np.pi*f*t*i)] for f in frequencies for i in range(1, self.num_harmonics+1)]
-        self.references = np.array(x).reshape(len(frequencies), 2*self.num_harmonics, int(samples))  
+        self.references = np.array(x).reshape(len(frequencies), 2*self.num_harmonics, self.samples)  
         
 
     
@@ -84,32 +97,39 @@ class SSVEPpredictor(OVBox):
                             print('Trial Start...')
                             self.command = None                             
                             self.trials_count += 1
+                            self.trial_ended = False
                         
                         if stim.identifier == OpenViBE_stimulation['OVTK_StimulationId_TrialStop']:
-                            print('Trial Stop... sending feedback')
+                            print('Trial Stop... ')
+                            self.trial_ended = True
                             # do feedback
-                            if self.do_feedback:
-                                print('Sending as feedback: ', self.command)
-                                self.feedback_socket.sendto(self.command, (self.hostname, self.feedback_port))
-                                self.do_feedback = False
-                          
+                            # while(self.do_feedback):
+                                # print('Sending as feedback: ', self.command)
+                                # self.feedback_socket.sendto(self.command, (self.hostname, self.feedback_port))
+                                # print('time of sending: ', self.getCurrentTime())
+                                # logging.info('feedback sent')
+                                # self.do_feedback = False                          
 
         if self.input[0]:
             buffer = self.input[0].pop()
             if type(buffer) == OVSignalBuffer:
-                if (buffer):
-                    samples = int(self.epoch_duration * self.fs)
-                    channels = int(len(buffer) / samples)
-                    epoch = np.array(buffer).reshape(channels, samples)
+                if (buffer):                    
+                    channels = int(len(buffer) / self.samples)
+                    epoch = np.array(buffer).reshape(channels, self.samples)
                     r = apply_cca(epoch, self.references)
                     command = predict(r)
-                    # self.command = str(self.frequencies[command+1])
                     self.command = str(command+1)
                     self.do_feedback = True
                     print('Frequency detected %s Hz' %(self.frequencies[command+1]))  
+
+        if self.trial_ended and self.do_feedback:
+            print('Sending as feedback: ', self.command)
+            self.feedback_socket.sendto(self.command, (self.hostname, self.feedback_port))
+            # print('time of sending: ', self.getCurrentTime())
+            logging.info('feedback sent')
+            self.do_feedback = False
+
                     
-
-
 
     
     def uninitialize(self):
