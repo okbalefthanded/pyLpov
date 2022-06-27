fdfe1c858c69eba6ed3f20ce83175c199fe657a5
okba bekhelifi <okba.bekhelifi@gmail.com>
2022-06-27 13:06:38 +0200

    - fixed proba score in evaluate when its dim is 2  On branch py3 Your branch is up to date with 'origin/py3'.
     Changes to be committed:
    	modified:   pyLpov/benchmark/benchmark_utils.py

--- pyLpov/benchmark/benchmark_utils.py
+++ pyLpov/benchmark/benchmark_utils.py
@@ -34,6 +34,8 @@ def evaluate(ds_test, clf, verbose=0):
     # acc = accuracy_score(ds_test.y.squeeze(), pred) * 100
     acc = balanced_accuracy_score(ds_test.y.squeeze(), pred) * 100
     # auc_score = roc_auc_score(ds_test.y.squeeze(), pred)
+    if score.ndim > 1:
+        score = score[:, 1]
     auc_score = roc_auc_score(ds_test.y.squeeze(), score) 
     pres = precision_score(ds_test.y.squeeze(), pred) * 100
     phrase = ds_test.events[0, ds_test.y[0] == 1]
